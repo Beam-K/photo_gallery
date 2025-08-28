044765abf6f10c1d6658e00b8bf82bb8e5143f48
Beam-K <buldinakata5@gmail.com>
2025-08-29 00:19:44 +0700

    The headline is ready
    we need to finalize the input styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.png';
+import iconSearch from './iconSearch.svg';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <div className="App-search">
+                    <img src={iconSearch} className={iconSearch} alt={iconSearch}/>
+                    <input className="App-search-input" type="search" placeholder="Search..."/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
